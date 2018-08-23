93910e971f2674ebb7bd559061c7a9806673ab9b
Poul Kjeldager Sørensen <pks@s-innovations.net>
2018-08-23 17:42:16 +0200

    repro for issue with 2.6

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CNTK;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,26 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            // The code provided will print ‘Hello World’ to the console.
+            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+            Console.WriteLine("Hello World!");
+
+
+
+            var deviceDescriptor = DeviceDescriptor.CPUDevice;
+            var model = Function.Load("FERPlus.onnx", deviceDescriptor, ModelFormat.ONNX);
+            Console.WriteLine("loaded");
+
+            Variable inputVar = model.Arguments.Single();
+
+            NDShape inputShape = inputVar.Shape;
+            int imageWidth = inputShape[0];
+            int imageHeight = inputShape[1];
+
+            Console.WriteLine($"{imageWidth}x{imageHeight}");
+
+            Console.ReadKey();
+
         }
     }
 }
